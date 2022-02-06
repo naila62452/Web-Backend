ffc4c96e8d87effc0c80a902665dcbd6bbabe783
naila62452 <naila.nosheen.uos@gmail.com>
2022-02-06 13:12:43 +0100

    Register user api
    User can register by using email and username

---
+++ server.js
@@ -0,0 +1,22 @@
+// const express = require('express')
+// const app = express()
+
+// app.listen(3000, () => console.log('Server started'))
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+const mongoose = require('mongoose');
+const users = require('./src/routes/users');
+const express = require('express');
+const app = express();
+
+mongoose.connect('mongodb://localhost/')
+    .then(() => console.log('Now connected to MongoDB!'))
+    .catch(err => console.error('Something went wrong', err));
+
+app.use(express.json());
+app.use('/register/users', users);
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+ 
+ 
