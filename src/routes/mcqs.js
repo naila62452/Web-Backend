09d96668a356727b2746375771adcee85ddf76b6
naila62452 <naila.nosheen.uos@gmail.com>
2022-02-15 13:22:31 +0100

    Mcqs update route

--- src/routes/mcqs.js
+++ src/routes/mcqs.js
@@ -30,11 +30,7 @@ function CreateQuestion(req, res) {
     // save mcqs in the database.
     mcqs.save()
         .then(data => {
-            res.send(
-                // success: true,
-                // message: 'Mcqs successfully created',
-                data
-            );
+            res.send(data);
         }).catch(err => {
             res.status(500).send({
                 success: false,
@@ -45,18 +41,13 @@ function CreateQuestion(req, res) {
 
 // retrieve and return all mcqs.
 function allMcqs_questions(req, res) {
-    console.log("hi")
     Mcqs.find()
         .then(data => {
             var message = "";
             if (data === undefined || data.length == 0) message = "No Mcqs found!";
             else message = 'Mcqs successfully retrieved';
 
-            res.send({
-                success: true,
-                message: message,
-                data: data
-            });
+            res.send(data);
         }).catch(err => {
             res.status(500).send({
                 success: false,
@@ -72,19 +63,18 @@ function mcqs_delete(req, res) {
             if (!data) {
                 return res.status(404).send({
                     success: false,
-                    message: "Shop not found with id " + req.params.id
+                    message: "Mcqs not found with id " + req.params.id
                 });
             }
             res.send({
                 success: true,
-                message: "Shop successfully deleted!"
+                message: "Mcqs successfully deleted!"
             });
         })
 };
 
 // find a single mcqs with a id.
 function mcqs_details(req, res) {
-    console.log("hello")
     Mcqs.findById(req.params.id)
         .then(data => {
             if (!data) {
@@ -93,18 +83,53 @@ function mcqs_details(req, res) {
                     message: "Mcqs not found with id " + req.params.id
                 });
             }
+            res.send(data);
+        })
+};
+
+// update a Mcqs  by the id.
+function mcqs_update (req, res) {
+    // validate request
+    if(!req.body.question || !req.body.options) {
+        return res.status(400).send({
+            success: false,
+            message: "Please enter All details"
+        });
+    }
+
+    // find Mcqs and update
+    Mcqs.findByIdAndUpdate(req.params.id, {
+        $set: req.body
+    }, {new: true})
+        .then(data => {
+            if(!data) {
+                return res.status(404).send({
+                    success: false,
+                    message: "Mcqs not found with id " + req.params.id
+                });
+            }
             res.send({
                 success: true,
-                message: 'Mcqs successfully retrieved',
                 data: data
             });
-        })
+        }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                success: false,
+                message: "Mcqs not found with id " + req.params.id
+            });
+        }
+        return res.status(500).send({
+            success: false,
+            message: "Error updating Mcqs with id " + req.params.id
+        });
+    });
 };
 
 //Routes
 router.get('/', allMcqs_questions);
 router.post('/create', CreateQuestion);
 router.get('/:id', mcqs_details);
-// router.put('/update/:id', mcqs_update);
+router.put('/update/:id', mcqs_update);
 router.delete('/delete/:id', mcqs_delete);
 module.exports = router;
